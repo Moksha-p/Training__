e1c6a5c5fa41d151571d0b2d5988d101730a070a
sudhanshuTech12 <sudhanshu@technmanconsulting.com>
2025-01-13 15:52:46 -0500

    output change

--- strings1.py
+++ strings1.py
@@ -3,4 +3,4 @@ def longest_word(sentence):
     longest = max(words, key=len)
     return longest
 
-print(longest_word("The quick brown fox jumps over the lazy dog"))  # Output: "jumps"
+print(longest_word("The quick brown fox jumps over the lazy dog"))  # Output: "quick"
