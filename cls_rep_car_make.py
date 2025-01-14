0cc64909c86b35158344a6b6fa447f7da44e2b74
deepakjape <deepak.j@technmanconsulting.com>
2025-01-14 16:43:14 -0500

    car

---
+++ cls_rep_car_make.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def __repr__(self):
+        return f"Make: {self.make} , Model: {self.model} , Year: {self.year}"
+    
+t1 = Car(make = "Toyota", model = "Camry", year = 2020)
+print(t1)
