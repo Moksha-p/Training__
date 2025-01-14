1fff46aeec1fa4d0617eff925c64acffbb2e3927
deepakjape <deepak.j@technmanconsulting.com>
2025-01-14 15:57:34 -0500

    emp_detail

---
+++ cls_to_rep_emp_frm_csv.py
@@ -0,0 +1,31 @@
+import csv
+class Employee:
+    def __init__(self,name,position,salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+    
+    def __repr__(self):
+        return f"Employee(Name: {self.name} , Position: {self.position} , Salary: {self.salary})"
+    
+def read_frm_csv(file):
+    employee_data = []
+    with open(file, mode='r', newline='') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            employee = Employee(
+                name = row['Name'],
+                position = row['Position'],
+                salary = row['Salary']
+
+            )
+            employee_data.append(employee)
+    return employee_data
+    
+file = "emp_detail.csv"
+employees = read_frm_csv(file)
+t2 = Employee(employees)
+for employee in employees:
+    print(employee)
+
+
